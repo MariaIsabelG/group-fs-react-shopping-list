c9eb92b2349690f47e8a683b42145a1abc73d3f1
Maria Isabel Gonzalez <isabel_133@hotmail.com>
2022-07-07 13:22:03 -0500

    added database sql

--- database.sql
+++ database.sql
@@ -1,2 +1,11 @@
 -- Don't forget to add your create table SQL 
+CREATE TABLE shopping_list (
+	id SERIAL PRIMARY KEY,
+	"name" varchar(80) NOT NULL,
+    "quantity" decimal (10,2) NOT NULL,
+    "unit" varchar(20),
+);
+
+
 -- It is also helpful to include some test data
+INSERT INTO "shopping_list" ("name","quantity","unit") VALUES ('Tomatoes', 3, 'each'), ('Cucumber', 4, 'each');
