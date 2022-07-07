c6d9f0eccc9e30e159e8f06bcab3beaff7663ac6
Maria Isabel Gonzalez <isabel_133@hotmail.com>
2022-07-07 13:56:08 -0500

    created a GET request

--- server/routes/list.router.js
+++ server/routes/list.router.js
@@ -3,6 +3,20 @@ const router = express.Router();
 const pool = require('../modules/pool.js');
 
 // TODO - Add routes here...
+// GET items
+router.get('/', (req, res) => {
+
+    const sqlText = `SELECT * FROM "shopping_list"`;
+    pool.query(sqlText)
+        .then((result) => {
+            console.log(result);
+        res.send(result.rows);
+        })
+        .catch((error) => {
+        console.log(`Error making database query ${sqlText}`, error);
+        res.sendStatus(500);
+        });
+});
 
 
 module.exports = router;
--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,10 +1,35 @@
-import React from 'react';
-
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 import Header from '../Header/Header.jsx'
 import './App.css';
 
-
 function App() {
+
+    let [itemList, setItemList] = useState([]);
+
+    useEffect(() => {
+    
+        getItems()
+
+    }, [])
+
+    const getItems = () => {
+        axios.get('/list')
+        .then(function (response) {
+            console.log(response.data)
+            setItemList( response.data )
+            }).catch( function (error) {
+                console.log( 'error in GET', error)
+            })
+    };
+
+
+
+
+
+
+
+
     return (
         <div className="App">
             <Header />
