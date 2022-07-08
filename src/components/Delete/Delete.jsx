b59f0f26586b5ceca0d5c5f1d6c090eaa70a745d
Isaac Brist <isaacbrist@gmail.com>
2022-07-08 10:15:30 -0500

    Mostly fixed delete function.

---
+++ src/components/Delete/Delete.jsx
@@ -0,0 +1,14 @@
+// import axios from 'axios';
+// function StudentList({studentList, getStudents}) {
+//     const deleteItem = (item) =>{
+//         console.log('Here is the item.id', item)
+//         axios.delete(
+//           `/list/${item}`
+//         ).then(function(response) {
+//             console.log('here is the response', response)
+//           getItems()
+//         }).catch(function(error) {
+//           alert('Something went wrong in the DELETE /list :(', error)
+//         })
+//       }
+//     }
